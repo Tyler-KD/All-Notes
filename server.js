14d7d22b5727df4a915c80332d673b4993c2c508
unknown <tyler.kd.knapp@gmail.com>
2024-02-02 11:21:12 -0600

    Installed dependencies, created Server.js file and gitignore

---
+++ server.js
@@ -0,0 +1,8 @@
+// Import Express.js
+const express = require('express');
+
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
+// Initialize an instance of Express.js
+const app = express();
