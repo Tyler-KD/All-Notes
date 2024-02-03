247ca27cfe0a988fb1d9dab2f4015aa6ac11f691
unknown <tyler.kd.knapp@gmail.com>
2024-02-03 10:33:31 -0600

    Created HTML and API routes within routes folder

---
+++ routes/api-routes.js
@@ -0,0 +1,7 @@
+// Imports express
+const router = require('express').Router();
+
+
+
+// Exports the app
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 // Import Express.js
 const express = require('express');
 
+// Import the html routes
+const htmlRoutes = require('./routes/html-routes')
+
+// Import the api routes
+const apiRoutes = require('./routes/api-routes')
+
 // Specify on which port the Express.js server will run
 const PORT = 3001;
 
 // Initialize an instance of Express.js
 const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
+
+// Middleware to use html routes
+app.use(htmlRoutes)
+
+// Middleware to use api routes
+app.use(apiRoutes)
+
+// listen() method is responsible for listening for incoming connections on the specified port
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
