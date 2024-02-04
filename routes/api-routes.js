a29279e819581807a3e440ff4b698bd636d44bbd
unknown <tyler.kd.knapp@gmail.com>
2024-02-04 09:33:12 -0600

    Added findIndex() method and splice() method to api DELETE Route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -44,11 +44,13 @@ api.post('/api/notes', (req, res) => {
     })
 });
 
-api.delete('/api/notes:id', (req, res) => {
+api.delete('/notes/:id', (req, res) => {
     console.info(`${req.method} request received for /api/notes:id`)
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         const allNotes = JSON.parse(data)
-
+        allNotes.findIndex(req.params.id).splice(req.params.id)
+        fs.writeFileSync('/db/db.json', JSON.stringify(allNotes))
+        res.json('note deleted')
     })
 })
 
